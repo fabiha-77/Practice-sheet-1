ea4e4e5383ed81fd9be5f0a4712e20798e55fa0d
fabiha-77 <fabiharashed@gmail.com>
2026-02-06 20:29:13 +0600

    Add files via upload

---
+++ assignment 1.c
@@ -0,0 +1,15 @@
+#include<cs50.h>
+#include<stdio.h>
+int main(void)
+{
+int a= get_int ("Enter first integar: ");
+    int b= get_int("Enter second integar: ");
+
+        printf("Sum: %d\n",a+b);
+            printf("difference: %d\n",a-b);
+                printf("product: %d\n",a*b);
+                    printf("quotient: %d\n",a/b);
+                        printf("reminder:%d\n",a%b);
+                        return 0;
+                    }
+
