99217c2f415bca3c849b4296dde49f906d52b12c
fabiha-77 <fabiharashed@gmail.com>
2026-02-06 20:33:36 +0600

    Add files via upload

---
+++ assignment 19.c
@@ -0,0 +1,9 @@
+#include<cs50.h>
+#include<stdio.h>
+int main()
+{
+    int n=get_int ("Enter thye value of n: ");
+        for (int i=1; i<=n; i++)
+            printf("%d\n",i*3);
+                return 0;
+                }
