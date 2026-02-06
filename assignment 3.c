736a378ea28b0e852c365098621dff4597f1afe7
fabiha-77 <fabiharashed@gmail.com>
2026-02-06 20:30:44 +0600

    Add files via upload

---
+++ assignment 3.c
@@ -0,0 +1,15 @@
+#include<cs50.h>
+#include<stdio.h>
+int main()
+{
+    int n = get_int("Enter a number: ");
+        if (n%2==0)
+
+                printf("Even\n");
+
+                else
+
+                    printf("odd\n");
+
+                    return 0;
+                    }
