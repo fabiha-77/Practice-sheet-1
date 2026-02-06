805df99312427cc0b2ed9e402b1843f2961aeebf
fabiha-77 <fabiharashed@gmail.com>
2026-02-06 20:35:38 +0600

    Add files via upload

---
+++ assignment 27.c
@@ -0,0 +1,16 @@
+#include<cs50.h>
+#include<stdio.h>
+int main()
+{
+    int number = 1;
+        for (int i=1; i<=4; i++)
+            {
+                    for (int j = 1; j <= i; j++)
+                            {
+                                        printf("%d",number);
+                                                    number ++;
+                                                            }
+                                                                    printf("\n");
+                                                                        }
+                                                                            return 0;
+                                                                            }
